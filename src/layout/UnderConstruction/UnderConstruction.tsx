e820e3dbd23067809fdfab95d20f865aa93b8934
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-25 09:41:11 -0400

    feat: remove unnecesary header and footer from under construction page

--- src/layout/UnderConstruction/UnderConstruction.tsx
+++ src/layout/UnderConstruction/UnderConstruction.tsx
@@ -1,11 +1,3 @@
-import Header from "@/components/Header/Header";
-import React from "react";
-
 export default function UnderConstruction() {
-  return (
-    <div className="flex flex-col gap-4 px-96 py-16">
-      <Header />
-      <h1 className="mt-4 text-4xl">🚧 This page is under construction</h1>
-    </div>
-  );
+  return <h1 className="mt-4 text-4xl">🚧 This page is under construction</h1>;
 }
