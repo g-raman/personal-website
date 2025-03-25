38e047a5eef3256d15a94bea2f763c64fcd64bf9
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-25 08:14:40 -0400

    feat: add links to github and linkedin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,12 @@ export default function Header() {
     <div className="flex items-baseline justify-between">
       <p className="text-4xl font-bold">Raman Gupta</p>
       <div className="flex gap-4">
-        <FontAwesomeIcon icon={faGithub} size="2xl" />
-        <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+        <a href="https://github.com/g-raman" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>
+        <a href="https://linkedin.com/in/gupta-raman" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+        </a>
       </div>
     </div>
   );
