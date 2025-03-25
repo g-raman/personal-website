b4d60f21dff37d6a48522f737d3d105f3c07fab0
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-25 11:00:42 -0400

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,20 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Personal Website | Raman Gupta",
   description: "Raman Gupta's Personal Website",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon-light.svg",
+        href: "/favicon-light.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon-dark.svg",
+        href: "/favicon-dark.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
