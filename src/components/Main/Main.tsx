d3905e04b0976fbb176d1a0dd83f7c6073be4bd2
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-25 08:20:55 -0400

    feat: add basic main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="text-lg/loose">
+      Hey, I&apos;m Raman :)
+      <br />
+      I write software that solves real-world problems
+      <br />
+      Always open to new opportunities, feel free to reach out!
+    </main>
+  );
+}
