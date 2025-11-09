234b1c3bead5cd6b4fe087bedeac19f16943e809
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 12:17:11 -0500

    feat: add basic nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav class="flex w-full items-baseline justify-between px-96 py-16">
+	<p class="text-2xl font-semibold">Raman Gupta</p>
+
+	<ul class="flex gap-4 text-xl font-light">
+		<li>
+			<p>About</p>
+		</li>
+
+		<li>
+			<p>Blog</p>
+		</li>
+	</ul>
+</nav>
