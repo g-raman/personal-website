6d2492b12dc5766a50ad2a0b510720f514cadcb1
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 14:20:14 -0500

    tweak: adjust container padding across various screen sizes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="bg-dot-grid h-dvh px-84 py-16">
+<div class="bg-dot-grid h-dvh px-8 py-8 md:px-24 md:py-16 lg:px-48 xl:px-64">
 	<div
 		class="flex h-full flex-col rounded-md border border-slate-500 bg-white bg-cover bg-center p-8"
 	>
