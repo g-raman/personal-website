5cbc3195793dcaf466d4d2c4e8b8439ce9c70b07
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 12:01:36 -0500

    cleanup: remove all project files

--- README.md
+++
@@ -1,6 +0,0 @@
-# Personal Website
-This is Raman Gupta's personal website.
-
-Showcases projects.
-
-Built with Next.js, TailwindCSS and hosted on Vercel.
--- src/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-import UnderConstruction from "@/layout/UnderConstruction/UnderConstruction";
-
-export default function About() {
-  return <UnderConstruction />;
-}
