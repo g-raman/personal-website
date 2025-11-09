372efc6fc07536d502ea391e3f6debf76dde3969
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 13:01:23 -0500

    feat: add github and linkedin icon to profile card

--- src/lib/components/ProfileCard.svelte
+++ src/lib/components/ProfileCard.svelte
@@ -1,3 +1,8 @@
+<script>
+	import { faGithub, faSquareLinkedin } from '@fortawesome/free-brands-svg-icons';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+</script>
+
 <section class="mb-8 flex flex-col items-center gap-8 md:flex-row">
 	<div class="flex flex-col items-center gap-4 md:items-start">
 		<p class="text-center text-2xl font-bold sm:text-4xl md:text-left">
@@ -7,8 +12,13 @@
 		</p>
 
 		<div class="flex gap-4 text-lg">
-			<a href="https://github.com/g-raman" target="_blank">GitHub</a>
+			<a href="https://github.com/g-raman" target="_blank">
+				<FontAwesomeIcon icon={faGithub} />
+				GitHub
+			</a>
+
 			<a class="text-[#0b66c2]" href="https://linkedin.com/in/gupta-raman" target="_blank">
+				<FontAwesomeIcon icon={faSquareLinkedin} />
 				LinkedIn
 			</a>
 		</div>
