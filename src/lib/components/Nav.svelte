f0743f2f3d885c44833a52d1d939cf53fd6b677d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 13:17:09 -0500

    feat: add hover styles for nav links

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,13 +1,13 @@
 <nav class="flex w-full items-baseline justify-between pb-16">
-	<a href="/" class="text-2xl font-semibold">Raman Gupta</a>
+	<a href="/" class="text-2xl font-semibold hover:text-current/70">Raman Gupta</a>
 
 	<ul class="flex gap-4 text-xl font-light">
 		<li>
-			<a href="/about">About</a>
+			<a class="hover:underline" href="/about">About</a>
 		</li>
 
 		<li>
-			<a href="/blog">Blog</a>
+			<a class="hover:underline" href="/blog">Blog</a>
 		</li>
 	</ul>
 </nav>
