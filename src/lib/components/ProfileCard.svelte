0edda7e130d733d0c4b68a33df238e9c862d0c7b
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-09 12:50:15 -0500

    feat: add profile card

---
+++ src/lib/components/ProfileCard.svelte
@@ -0,0 +1,16 @@
+<section class="mb-8 flex flex-col items-center gap-8 md:flex-row">
+	<div class="flex flex-col items-center gap-4 md:items-start">
+		<p class="text-center text-2xl font-bold sm:text-4xl md:text-left">
+			<span>Hey, I&apos;m Raman.</span>
+			<br />
+			<span class="text-gray-500">I&apos;m a Software Developer.</span>
+		</p>
+
+		<div class="flex gap-4 text-lg">
+			<a href="https://github.com/g-raman" target="_blank">GitHub</a>
+			<a class="text-[#0b66c2]" href="https://linkedin.com/in/gupta-raman" target="_blank">
+				LinkedIn
+			</a>
+		</div>
+	</div>
+</section>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as Nav } from './components/Nav.svelte';
 export { default as Projects } from './components/Projects.svelte';
+export { default as ProfileCard } from './components/ProfileCard.svelte';
