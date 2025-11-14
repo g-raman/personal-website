2ac645a78ee83cfa8137f08267c4e289d2d18fbd
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-14 07:24:37 -0500

    feat: add page for listing all blog posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,18 @@
-<h1 class="text-3xl font-bold md:text-5xl">Under construction</h1>
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
+<section>
+	<ul class="flex flex-col gap-4">
+		{#each data.posts as post (post.slug)}
+			<li class="flex items-baseline gap-4">
+				<p class="text-base text-gray-500">{post.metadata.publishedAt}</p>
+				<a href={post.slug} class="text-lg underline">{post.metadata.title}</a>
+			</li>
+		{/each}
+	</ul>
+</section>
