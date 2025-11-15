e141c4b71e9097583289a9dea9c48125b572fd5a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-15 13:39:33 -0500

    feat: create basic renderer for blog

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { formatDate } from '$lib';
+
+	let { data } = $props();
+	const Content = data.content;
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published at {formatDate(data.meta.publishedAt)}</p>
+	</hgroup>
+
+	<div class="prose">
+		<Content />
+	</div>
+</article>
