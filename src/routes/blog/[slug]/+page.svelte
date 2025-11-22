047c48fad9db056475b5e3b3dd14b53d80ec47fd
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-21 21:24:20 -0500

    fix: adjust margin between article and footer

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -11,7 +11,7 @@
 	<meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<article>
+<article class="mb-8">
 	<hgroup>
 		<h1 class="mb-2 text-4xl font-bold">{data.meta.title}</h1>
 		<p class="text-stone-500">{formatDateFriendly(data.meta.publishedAt)}</p>
