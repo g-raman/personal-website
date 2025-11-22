9d1b2f9d0f5040654420856c76c90a675badb66b
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-21 23:17:11 -0500

    feat: remove padding on y axis for container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,12 +29,8 @@
 	<link rel="icon" type="image/svg" href={faviconDark} media="(prefers-color-scheme: dark)" />
 </svelte:head>
 
-<div
-	class="bg-dot-grid flex min-h-dvh flex-col bg-[#fffcf0] px-8 py-8 md:px-24 md:py-16 lg:px-48 xl:px-84"
->
-	<div
-		class="flex flex-1 flex-col rounded-md border border-stone-500 bg-[#fffcf0] bg-cover bg-center p-8"
-	>
+<div class="bg-dot-grid flex min-h-dvh flex-col bg-[#fffcf0] px-8 md:px-24 lg:px-48 xl:px-84">
+	<div class="flex flex-1 flex-col border-x border-stone-500 bg-[#fffcf0] bg-cover bg-center p-8">
 		<Nav />
 
 		<main>
