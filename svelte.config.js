e0e9d3b2e0878a185a8de6991c5846620306b366
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-22 09:24:21 -0500

    refactor: move mdsvex config to separate file

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,11 @@
-import { escapeSvelte, mdsvex } from 'mdsvex';
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { createHighlighter } from 'shiki';
-
-async function highlighter(code, lang) {
-	const highlighter = await createHighlighter({
-		langs: [lang],
-		themes: ['kanagawa-wave']
-	});
-
-	const html = escapeSvelte(highlighter.codeToHtml(code, { theme: 'kanagawa-wave', lang }));
-
-	return `{@html \`${html}\` }`;
-}
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-		mdsvex({
-			extensions: ['.md'],
-			highlight: { highlighter }
-		})
-	],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.md']
 };
