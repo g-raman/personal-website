0952cd0359681cc16b0108d959b7af9206a0203a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-21 22:54:15 -0500

    feat: add copy code button

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,4 @@ export { default as Projects } from './components/Projects.svelte';
 export { default as ProfileCard } from './components/ProfileCard.svelte';
 export { default as Footer } from './components/Footer.svelte';
 export { formatDateFriendly, formatDateNumeric } from './utils/dates';
+export { default as CopyCodeButton } from './components/CopyCodeButton.svelte';
