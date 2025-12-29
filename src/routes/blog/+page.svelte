a8e05010070f3dea11c4a461da3987e28203186d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-12-29 10:32:52 -0500

    fix: adjust styling of blog list page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -13,8 +13,10 @@
 	<h1 class="mb-4 text-4xl font-bold">Posts</h1>
 	<ul class="flex flex-col gap-4">
 		{#each data.posts as post (post.slug)}
-			<li class="flex items-baseline gap-4">
-				<p class="text-base text-stone-500">{formatDateNumeric(post.metadata.publishedAt)}</p>
+			<li class="flex items-baseline gap-2">
+				<p class="w-24 text-base text-stone-500">
+					{formatDateNumeric(post.metadata.publishedAt)}
+				</p>
 				<a href={resolve(`/blog/${post.slug}`)} class="text-lg underline">{post.metadata.title}</a>
 			</li>
 		{/each}
